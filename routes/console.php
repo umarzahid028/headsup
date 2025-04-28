f68a3e27d47b7fb73a8eb9c90775e918d8c4ed0b
Umar Zahid <umarzahid028@gmail.com>
2025-04-28 16:41:23 +0500

    done

--- routes/console.php
+++ routes/console.php
@@ -5,4 +5,4 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
   
 
-Schedule::command('import:vehicles-csv --archive')->everyMinute();
+Schedule::command('import:vehicles-csv --archive')->everyMinute()->appendOutputTo(storage_path('logs/import-vehicles-csv.log'));
