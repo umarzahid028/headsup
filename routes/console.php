a317f0548c5965a9b4d68fa0c90a2b364d424eb4
Umar Zahid <umarzahid028@gmail.com>
2025-04-28 18:51:54 +0500

    path

--- routes/console.php
+++ routes/console.php
@@ -5,4 +5,4 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
   
 
-Schedule::command('import:vehicles-csv --archive')->everyMinute()->appendOutputTo(storage_path('logs/import-vehicles-csv.log'));
+Schedule::command('import:vehicles-csv --directory=/home/u529019370/domains/recon.trevinosauto.com/inv_up_dc --archive')->everyMinute()->appendOutputTo(storage_path('logs/import-vehicles-csv.log'));
