11322252f0796f84ff5b7b5824db0dd01e7bfc2f
Your Name <youremail@example.com>
2025-06-23 14:14:34 +0500

    doen

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -71,11 +71,10 @@
             <div class="pt-2">
 
                 <a href="{{ route('saleperson.table') }}" class="{{ request()->routeIs('saleperson.table') ? 'bg-gray-100 text-gray-900' : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900' }} group flex items-center px-3 py-2 text-sm font-medium rounded-md">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" width="24" height="24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M18 9v6m3-3h-6m-4 4a4 4 0 100-8 4 4 0 000 8zm0 0v1a4 4 0 01-4 4H5a4 4 0 01-4-4v-1a6 6 0 0112 0z" />
-                    </svg>
-                    <span class="flex-1" style="margin-left: 5px;">Sales Person</span>
+                   <svg xmlns="http://www.w3.org/2000/svg" class="text-gray-500 mr-3 flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path>
+                            </svg>
+                    <span class="flex-1" style="margin-left: 5px;">Users</span>
                 </a>
             </div>
             @endhasrole
